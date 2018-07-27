852b75b9d77dc23b130d99e77cf5bc74b2e31ed3
Tai Tran <tranthanhtai257@gmail.com>
2018-07-27 15:27:13 +0700

    static_pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def full_title page_title = ""
+    base_title = "Chia sẻ thông tin - Tìm kiếm phòng trọ"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
 end
