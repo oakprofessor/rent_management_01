7d52dc6f26be4397b77a70e09dc71a3dad20e9a2
Tai Tran <tranthanhtai257@gmail.com>
2018-07-31 13:28:54 +0700

    create_menubar_detail

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   root "static_pages#home"
-  get "static_pages/home"
+  get  "/room", to: "static_pages#room"
+  get  "/news", to: "static_pages#news"
+  get  "/contact", to: "static_pages#contact"
+  get  "/help", to: "static_pages#help"
 end
