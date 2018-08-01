3d15dbc8ceaaaec64423f8fa1600df86a49282d8
Phuc dog <onthidaihocabd@gmail.com>
2018-08-01 13:31:18 +0700

    signup login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
   has_many :homes
   has_many :role_users
   has_many :orders
@@ -16,4 +17,31 @@ class User < ApplicationRecord
   validates :phone, numericality: true, presence: true
   validates :stk, numericality: true
   validates :address, presence: true
+
+  def self.digest string
+    cost = if ActiveModel::SecurePassword.min_cost
+             BCrypt::Engine::MIN_COST
+           else
+             BCrypt::Engine.cost
+           end
+    BCrypt::Password.create(string, cost: cost)
+  end
+
+  def self.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def remember
+    self.remember_token = User.new_token
+    update_attribute :remember_digest, User.digest(remember_token)
+  end
+
+  def authenticated? remember_token
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  def forget
+    update_attribute :remember_digest, nil
+  end
 end
