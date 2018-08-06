e0ba6374dd642ef617e8a89b09e017a5111c9821
hp <donghungfeng@ubuntu.ubuntu-domain>
2018-08-06 03:06:40 -0400

    0

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_05_185401) do
+ActiveRecord::Schema.define(version: 2018_08_06_055800) do
 
   create_table "facilities", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
